2cf0ed69cf97e6863f0e251c3c07b526f22a45bd
AppWerft <rs@hamburger-appwerft.de>
2013-09-11 17:23:14 +0200

        

--- src/de/appwerft/apponsor/ExampleProxy.java
+++ src/de/appwerft/apponsor/ExampleProxy.java
@@ -21,47 +21,43 @@ import org.appcelerator.titanium.view.TiUIView;
 
 import android.app.Activity;
 
-
 // This proxy can be created by calling Tiapponsor.createExample({message: "hello world"})
-@Kroll.proxy(creatableInModule=TiapponsorModule.class)
-public class ExampleProxy extends TiViewProxy
-{
+@Kroll.proxy(creatableInModule = TiapponsorModule.class)
+public class ExampleProxy extends TiViewProxy {
 	// Standard Debugging variables
 	private static final String TAG = "ExampleProxy";
 
-	private class ExampleView extends TiUIView
-	{
+	private class ExampleView extends TiUIView {
 		public ExampleView(TiViewProxy proxy) {
 			super(proxy);
 			LayoutArrangement arrangement = LayoutArrangement.DEFAULT;
 
 			if (proxy.hasProperty(TiC.PROPERTY_LAYOUT)) {
-				String layoutProperty = TiConvert.toString(proxy.getProperty(TiC.PROPERTY_LAYOUT));
+				String layoutProperty = TiConvert.toString(proxy
+						.getProperty(TiC.PROPERTY_LAYOUT));
 				if (layoutProperty.equals(TiC.LAYOUT_HORIZONTAL)) {
 					arrangement = LayoutArrangement.HORIZONTAL;
 				} else if (layoutProperty.equals(TiC.LAYOUT_VERTICAL)) {
 					arrangement = LayoutArrangement.VERTICAL;
 				}
 			}
-			setNativeView(new TiCompositeLayout(proxy.getActivity(), arrangement));
+			setNativeView(new TiCompositeLayout(proxy.getActivity(),
+					arrangement));
 		}
 
 		@Override
-		public void processProperties(KrollDict d)
-		{
+		public void processProperties(KrollDict d) {
 			super.processProperties(d);
 		}
 	}
 
 	// Constructor
-	public ExampleProxy()
-	{
+	public ExampleProxy() {
 		super();
 	}
 
 	@Override
-	public TiUIView createView(Activity activity)
-	{
+	public TiUIView createView(Activity activity) {
 		TiUIView view = new ExampleView(this);
 		view.getLayoutParams().autoFillsHeight = true;
 		view.getLayoutParams().autoFillsWidth = true;
@@ -70,32 +66,30 @@ public class ExampleProxy extends TiViewProxy
 
 	// Handle creation options
 	@Override
-	public void handleCreationDict(KrollDict options)
-	{
+	public void handleCreationDict(KrollDict options) {
 		super.handleCreationDict(options);
-		
+
 		if (options.containsKey("message")) {
-			Log.d(TAG, "example created with message: " + options.get("message"));
+			Log.d(TAG,
+					"example created with message: " + options.get("message"));
 		}
 	}
-	
+
 	// Methods
 	@Kroll.method
-	public void printMessage(String message)
-	{
+	public void printMessage(String message) {
 		Log.d(TAG, "printing message: " + message);
 	}
 
-
-	@Kroll.getProperty @Kroll.method
-	public String getMessage()
-	{
-        return "Hello World from my module";
+	@Kroll.getProperty
+	@Kroll.method
+	public String getMessage() {
+		return "Hello World from my module";
 	}
 
-	@Kroll.setProperty @Kroll.method
-	public void setMessage(String message)
-	{
-	    Log.d(TAG, "Tried setting module message to: " + message);
+	@Kroll.setProperty
+	@Kroll.method
+	public void setMessage(String message) {
+		Log.d(TAG, "Tried setting module message to: " + message);
 	}
 }
